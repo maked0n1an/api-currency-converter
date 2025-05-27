c4dbc84f59f78a0a1cf5a3494120eb0299f3310a
maked0n1an <promkapromka4146@gmail.com>
2025-05-27 17:31:49 +0200

    Added new validations for UserRegisterSchema and UserUpdateSchema.

--- src/api/schemas/user.py
+++ src/api/schemas/user.py
@@ -1,18 +1,63 @@
-from typing_extensions import TypedDict
 import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, ValidationInfo, field_validator
+from typing_extensions import TypedDict
 
 
 class UserRegisterSchema(BaseModel):
-    email: str
-    username: str
-    password: str
+    email: EmailStr = Field(..., description="Error in email")
+    username: str = Field(..., description="Username should be string")
+    password: str = Field(
+        min_length=12,
+        max_length=25,
+        description="Password must be 12-25 characters long and contain uppercase, lowercase, digit, and special character",
+    )
+    
+    @field_validator("username")
+    def validate_str(cls, value: str, info: ValidationInfo):
+        if not value.isalpha():
+            raise ValueError(
+                f"{info.field_name.capitalize()} should contain only letters"
+            )
+        return value
+
+
+    @field_validator("password")
+    def validate_password(cls, value: str):
+        if not any(c.isupper() for c in value):
+            raise ValueError(
+                "Password should contain at least one uppercase letter"
+            )
+
+        if not any(c.islower() for c in value):
+            raise ValueError(
+                "Password should contain at least one lowercase letter"
+            )
+
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password should contain at least one digit")
+
+        special_chars = "!@#$%'^&*()_-+={}[|]\\;:\"<>,./?~`"
+        if not any(c in special_chars for c in value):
+            raise ValueError(
+                "Password should contain at least one special character"
+            )
+
+        return value
 
 
 class UserUpdateSchema(BaseModel):
     first_name: str
     last_name: str
+
+    @field_validator("first_name", "last_name")
+    def validate_str(cls, value: str, info: ValidationInfo):
+        parts = value.split("-")
+        if not all(part.isalpha() for part in parts):
+            raise ValueError(
+                f"{info.field_name.capitalize()} should contain only letters"
+            )
+        return value
 
 
 class UserReturnSchema(BaseModel):
@@ -23,22 +68,18 @@ class UserReturnSchema(BaseModel):
     last_name: str | None
 
 
+class ExternalAuthUserCreateSchema(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+
+
 class UserRegisterResponse(BaseModel):
     id: uuid.UUID
     email: str
     username: str
 
 
-class ExternalAuthUserCreateSchema(BaseModel):
-    email: str
-    first_name: str
-    last_name: str
-    
-
 class UserFilter(TypedDict):
     email: str
     username: str
-    
-
-class GoogleUserCreateSchema(ExternalAuthUserCreateSchema):
-    pass
